a50a5829a50319b84ce7aa552a61a945659ea623
JeinKh <juliakhezhun@gmail.com>
2024-08-01 09:38:48 +0300

    ///

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,7 +1,8 @@
 import ImageCard from "../ImageCard/ImageCard";
+import s from "./ImageGallery.module.css";
 const ImageGallery = ({ items, onImageClick }) => {
   return (
-    <ul>
+    <ul className={s.wrapper}>
       {items.map((item) => (
         <li key={item.id}>
           <ImageCard item={item} onClick={onImageClick} />
